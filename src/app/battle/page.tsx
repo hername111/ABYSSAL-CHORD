74f93fc99781f56be73f4d773599d11c0aecf1dd
user8868802809 <1400138241676361-user8868802809@noreply.coze.cn>
2026-05-24 00:39:09 +0800

    fix: 调整敌人状态面板向左移动到25%位置
    Coze-Commit-Type: user
    Coze-User-ID: 1400138241676361
    Coze-Conversation-ID: 5072141

--- src/app/battle/page.tsx
+++ src/app/battle/page.tsx
@@ -1375,7 +1375,7 @@ export default function BattleArena() {
           </div>
           
           {/* 敌人状态面板 - 使用通用的 EntityStatusPanel */}
-          <div className="absolute -bottom-44 left-[65%]">
+          <div className="absolute -bottom-44 left-[25%]">
             <EntityStatusPanel 
               entity={enemyState} 
               isEnemy={true}
